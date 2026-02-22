2f372814c36bb3ba5a15dcae102cba2145b01b94
ebaek200 <57092245+ebaek200@users.noreply.github.com>
2026-02-22 21:05:34 +0900

    fix daewoon 60ganji logic

--- bazi_engine.py
+++ bazi_engine.py
@@ -1,92 +1,35 @@
-import sxtwl
-import sys
-import json
-
-year = int(sys.argv[1])
-month = int(sys.argv[2])
-day = int(sys.argv[3])
-hour = int(sys.argv[4])
-gender = sys.argv[5]
-
-day_obj = sxtwl.fromSolar(year, month, day)
-
-year_gz = day_obj.getYearGZ()
-month_gz = day_obj.getMonthGZ()
-day_gz = day_obj.getDayGZ()
-hour_gz = day_obj.getHourGZ(hour)
-
-stems = ["갑", "을", "병", "정", "무", "기", "경", "신", "임", "계"]
-branches = ["자", "축", "인", "묘", "진", "사", "오", "미", "신", "유", "술", "해"]
-
 # --------------------------
-# 순행 / 역행 결정
+# 🔥 60갑자 테이블 생성
 # --------------------------
-yang_stems = [0, 2, 4, 6, 8]
-is_yang_year = year_gz.tg in yang_stems
+ganji_60 = []
+for i in range(60):
+    ganji_60.append({
+        "stem": stems[i % 10],
+        "branch": branches[i % 12]
+    })
 
-if gender == "male":
-    forward = is_yang_year
-else:
-    forward = not is_yang_year
+# 월주의 60갑자 index 찾기
+month_ganji = stems[month_gz.tg] + branches[month_gz.dz]
 
-# --------------------------
-# 대운 시작 나이 (임시 유지)
-# --------------------------
-
-
-def get_next_jieqi_days():
-    for i in range(1, 40):
-        test = sxtwl.fromSolar(year, month, day + i)
-        if test.hasJieQi():
-            return i
-    return 0
-
-
-def get_prev_jieqi_days():
-    for i in range(1, 40):
-        test = sxtwl.fromSolar(year, month, day - i)
-        if test.hasJieQi():
-            return i
-    return 0
-
-
-if forward:
-    diff_days = get_next_jieqi_days()
-else:
-    diff_days = get_prev_jieqi_days()
-
-daewoon_start_age = diff_days // 3
+month_index_60 = 0
+for i in range(60):
+    if ganji_60[i]["stem"] + ganji_60[i]["branch"] == month_ganji:
+        month_index_60 = i
+        break
 
 # --------------------------
 # 🔥 대운 배열 생성
 # --------------------------
-month_index = month_gz.tg * 12 + month_gz.dz
-
 daewoon_list = []
 
 for i in range(1, 11):
     if forward:
-        index = month_index + i
+        idx = (month_index_60 + i) % 60
     else:
-        index = month_index - i
-
-    stem = stems[index % 10]
-    branch = branches[index % 12]
+        idx = (month_index_60 - i) % 60
 
     daewoon_list.append({
         "age": daewoon_start_age + (i - 1) * 10,
-        "stem": stem,
-        "branch": branch
+        "stem": ganji_60[idx]["stem"],
+        "branch": ganji_60[idx]["branch"]
     })
-
-result = {
-    "year": {"stem": stems[year_gz.tg], "branch": branches[year_gz.dz]},
-    "month": {"stem": stems[month_gz.tg], "branch": branches[month_gz.dz]},
-    "day": {"stem": stems[day_gz.tg], "branch": branches[day_gz.dz]},
-    "hour": {"stem": stems[hour_gz.tg], "branch": branches[hour_gz.dz]},
-    "daewoon_start_age": daewoon_start_age,
-    "direction": "순행" if forward else "역행",
-    "daewoon": daewoon_list
-}
-
-print(json.dumps(result, ensure_ascii=False))
